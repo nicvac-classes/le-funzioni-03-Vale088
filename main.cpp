b90c167bea8ca98e708e9a4fc9cbe88e5ea9548e
Vale088 <valerio.sinni@marconibari.edu.it>
2025-11-26 17:27:16 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,37 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  double calcarea(double raggio) {
+    double area;
+
+    area = 3.14 * raggio * raggio;
+    
+    return area;
+}
+
+double calccirc(double raggio) {
+    double circonferenza;
+
+    circonferenza = 2 * 3.14 * raggio;
+    
+    return circonferenza;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
